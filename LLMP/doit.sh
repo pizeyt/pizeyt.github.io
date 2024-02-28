8ce50b52a78bd3708db7e99694c9af713dc1802f
Tim Pizey <PIZEYT@science.regn.net>
2024-02-28 22:32:41 +0000

    Onwards

--- LLMP/doit.sh
+++ LLMP/doit.sh
@@ -1,6 +1,7 @@
-darkslide --embed --linenos=no --theme=themes/timp --relative --destination=LLMP_default.html config.cfg
-darkslide --embed --linenos=no --theme=abyss --relative --destination=LLMP_abyss.html config.cfg
-darkslide --embed --linenos=no --theme=void  --relative --destination=LLMP_void.html config.cfg
-darkslide --embed --linenos=no --theme=white --relative --destination=LLMP_white.html config.cfg
+darkslide --embed --linenos=no --theme=themes/timp --relative --destination=LLMP_default.html LLMP.md
+darkslide --embed --linenos=no --theme=abyss --relative --destination=LLMP_abyss.html LLMP.md
+darkslide --embed --linenos=no --theme=void  --relative --destination=LLMP_void.html LLMP.md
+darkslide --embed --linenos=no --theme=white --relative --destination=LLMP_white.html LLMP.md
+darkslide --embed --linenos=no --theme=white --relative --destination=LLMP_els.html LLMP.md
 git commit -m Onwards -a
 git push origin master 
